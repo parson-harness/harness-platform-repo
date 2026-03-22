b54ad1f9974cfa745de8e0bc43b681ef378af2af
Todd Parson <todd.parson@harness.io>
2026-03-22 17:24:29 -0500

    fix: add prodListenerRuleArn, stageListener, infrastructure/execution to AsgBlueGreenDeploy step

--- tofu/modules/harness-pipeline/asg-pipeline.tf
+++ tofu/modules/harness-pipeline/asg-pipeline.tf
@@ -99,12 +99,16 @@ resource "harness_platform_pipeline" "asg_strategy" {
                       identifier: asg_blue_green_deploy
                       type: AsgBlueGreenDeploy
                       timeout: 20m
+                      infrastructure: {}
+                      execution: {}
                       spec:
                         useAlreadyRunningInstances: false
                         loadBalancers:
                           - loadBalancer: <+infra.loadBalancers[0].loadBalancerName>
                             prodListener: <+infra.loadBalancers[0].prodListenerArn>
-                            stageListenerArn: <+infra.loadBalancers[0].stageListenerArn>
+                            prodListenerRuleArn: ""
+                            stageListener: <+infra.loadBalancers[0].stageListenerArn>
+                            stageListenerRuleArn: ""
                   - step:
                       name: Validate Stage
                       identifier: validate_stage_bg
