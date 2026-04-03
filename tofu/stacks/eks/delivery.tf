69f8e000fc348279276b9df7418859c7f5213d48
Todd Parson <todd.parson@harness.io>
2026-04-03 12:52:08 -0500

    Disable change governance for EKS delivery pipelines

--- tofu/stacks/eks/delivery.tf
+++ tofu/stacks/eks/delivery.tf
@@ -173,8 +173,9 @@ module "harness_pipelines" {
   harness_project_id     = local.resolved_project_id
   harness_api_key        = var.harness_api_key
 
-  delegate_selector = local.delegate_selector
-  pipeline_tags     = ["tofu-managed", var.owner, "eks"]
+  delegate_selector        = local.delegate_selector
+  enable_change_governance = false
+  pipeline_tags            = ["tofu-managed", var.owner, "eks"]
 
   # Standard CI Gradle pipeline (with security scanning and supply chain)
   # Include owner prefix for multi-sandbox support in same project
