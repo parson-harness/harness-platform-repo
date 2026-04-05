3dbf32b6f9f87c255756ac96d3d3b6b162ed2d00
Todd Parson <todd.parson@harness.io>
2026-04-05 16:09:01 -0500

    Use JUnit report object for tests-v2

--- tofu/modules/harness-pipeline/standard-ci-gradle.tf
+++ tofu/modules/harness-pipeline/standard-ci-gradle.tf
@@ -137,7 +137,11 @@ resource "harness_platform_pipeline" "standard_ci_gradle" {
                                   connectorRef: account.harnessImage
                                   image: gradle:8.5-jdk17
                                   intelligenceMode: true
-                                  reports: build/test-results/test/*.xml
+                                  reports:
+                                    type: JUnit
+                                    spec:
+                                      paths:
+                                        - build/test-results/test/*.xml
                             - step:
                                 type: Run
                                 name: Build Intelligence
